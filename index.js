cd0aac0d8cd1a2d30d99b4728b3998c1b671e5f3
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-10-09 01:44:44 +0300

    1

---
+++ index.js
@@ -0,0 +1,10 @@
+import menuItemTpl from "./templates/menuItem.hbs";
+import dishes from "./menu.json";
+console.log(menuItemTpl);
+
+const menuItemMarkup = createMenuItemMarkup(dishes);
+
+function createMenuItemMarkup(dishes) {
+  //   return dishes.map((dish) => menuItemTpl(dish)).join("");
+  return dishes.map(menuItemTpl).join("");
+}
