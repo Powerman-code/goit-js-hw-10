b50fefb07550f2e454487d906a450efe8ad02061
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-10-13 02:42:18 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import menuItemTpl from "../templates/menuItem.hbs";
+// импортируем шаблон
 import dishes from "./menu.json";
+// импортируем описание блюд
 console.log(menuItemTpl(dishes));
 
 const menuItemMarkup = createMenuItemMarkup(dishes);
+// Создаем переменную, в которую записываем результат вызова ф-и
+// которая создает разметку, и передаем в нее список блюд.
 const galaryList = document.querySelector('.js-menu');
 // console.log(galaryList);
 galaryList.insertAdjacentHTML("beforeend", menuItemMarkup);
-
+// вставляем
 function createMenuItemMarkup(dishes) {
   //   return dishes.map((dish) => menuItemTpl(dish)).join("");
   return dishes.map(menuItemTpl).join("");
